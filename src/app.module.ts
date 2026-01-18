d82bae59733ca3a18776cd13ab4494a1e3c8550e
Arifin Depay Jr <121757305+Arifindepayjr1@users.noreply.github.com>
2026-01-18 23:29:25 +0700

    chore: register queue and code runner modules in app

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,11 +2,16 @@ import { Module } from '@nestjs/common';
 import { UsersModule } from './modules/users/users.module';
 import { AuthModule } from './modules/auth/auth.module';
 import { PrismaService } from 'prisma/prisma.service';
-import { ClassroomsModule } from './modules/classrooms/classrooms.module';
-
+import { AppController } from './app.controller';
+import { QueueModule } from './modules/queue/queue.module';
+import { CodeRunnerModule } from './modules/code-runner/code-runner.module';
 @Module({
-  imports: [UsersModule, AuthModule, ClassroomsModule],
-  controllers: [],
+  imports: [
+    UsersModule,
+    AuthModule,
+    QueueModule, CodeRunnerModule
+  ],
+  controllers: [AppController],
   providers: [PrismaService],
 })
 export class AppModule {}
