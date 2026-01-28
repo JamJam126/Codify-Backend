e470a5a361aa3902c1baf52b6225798a98416967
VRithea <vongrithea126@gmail.com>
2026-01-28 10:54:29 +0700

    chore: add hosting environment configuration

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,8 +3,8 @@ import { UsersModule } from './modules/users/users.module';
 import { AuthModule } from './modules/auth/auth.module';
 import { PrismaService } from 'prisma/prisma.service';
 import { ConfigModule } from '@nestjs/config';
-import { QueueModule } from './modules/queue/queue.module';
-import { CodeRunnerModule } from './modules/code-runner/code-runner.module';
+// import { QueueModule } from './modules/queue/queue.module';
+// import { CodeRunnerModule } from './modules/code-runner/code-runner.module';
 import { ClassroomsModule } from './modules/classrooms/classrooms.module';
 import { AssignmentModule } from './modules/assignments/assignment.module';
 @Module({
@@ -14,7 +14,7 @@ import { AssignmentModule } from './modules/assignments/assignment.module';
     AuthModule,
     ClassroomsModule,
     AssignmentModule,
-    QueueModule, CodeRunnerModule
+    // QueueModule, CodeRunnerModule
   ],
   providers: [PrismaService],
 })
